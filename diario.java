5db2a541f50837b6cd457fac1bc18d3eb21dd2e1
ericlucena6485 <74722316+ericlucena6485@users.noreply.github.com>
2021-02-25 12:51:00 -0300

    Add files via upload

---
+++ diario.java
@@ -0,0 +1,57 @@
+package teste;
+
+public class diario {
+	private String local_viagem;
+	private String voo;
+	private int dia;
+	private int mes;
+	private int ano;
+	private String feedback;
+
+	public void setNome(String local_viagem) {
+		this.local_viagem = local_viagem;
+	}
+
+	public String getLocal_viagem() {
+		return local_viagem;
+	}
+
+	public void setVoo(String voo) {
+		this.voo = voo;
+	}
+
+	public String getVoo() {
+		return voo;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	public String getFeedback() {
+		return Feedback;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+}
