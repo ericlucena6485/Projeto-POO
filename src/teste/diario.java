909b6b7227441c9c3cab0bfb9206dcec5603cdf1
Deusdedit99 <64427453+Deusdedit99@users.noreply.github.com>
2021-03-01 10:01:41 -0300

    Create diario.java

---
+++ src/teste/diario.java
@@ -0,0 +1,60 @@
+package teste;
+
+public class diario extends login {
+	private String local_viagem;
+	private String voo;
+	private int dia;
+	private int mes;
+	private int ano;
+	private String feedback;
+
+	public void setLocal_viagem(String local_viagem) {
+		this.local_viagem = local_viagem;
+	}
+
+	
+	public String getLocal_viagem() {
+		return local_viagem;
+	}
+
+	public void setVoo(String voo) {
+		this.voo = voo;
+	}
+
+	public String getVoo() {
+		return voo;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+	
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+	
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+}
